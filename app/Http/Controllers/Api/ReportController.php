7f737208b7b401bcc48ed542aa1368c2d2553224
Sadra Moghadasi <sadra.19@gmail.com>
2024-08-21 08:56:28 +0330

    Fix bug report by driver

--- app/Http/Controllers/Api/ReportController.php
+++ app/Http/Controllers/Api/ReportController.php
@@ -3,20 +3,22 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Load;
 use App\Models\Report;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 class ReportController extends Controller
 {
     public function store(Request $request)
     {
+        $load = Load::findOrFail($request->load_id);
         $validator = Validator::make($request->all(), [
             'type' => 'required',
             'description' => 'required',
             'load_id' => 'required',
-            'driver_id' => 'required',
-            'owner_id' => 'required',
+            // 'owner_id' => 'required',
         ]);
         if ($validator->fails()) {
             return response()->json($validator->errors(), 422);
@@ -25,8 +27,8 @@ class ReportController extends Controller
         $report->type = $request->type;
         $report->description = $request->description;
         $report->load_id = $request->load_id;
-        $report->driver_id = $request->driver_id;
-        $report->owner_id = $request->owner_id;
+        $report->driver_id = Auth::id();
+        $report->owner_id = $load->user_id;
         $report->save();
 
         return response()->json('OK', 200);
