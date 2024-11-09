71e4a0a14466112d5289ea122c36cf7f55545b64
Sadra Moghadasi <sadra.19@gmail.com>
2024-11-09 09:40:35 +0330

    update transaction

--- app/Http/Controllers/Api/TransactionController.php
+++ app/Http/Controllers/Api/TransactionController.php
@@ -12,8 +12,17 @@ class TransactionController extends Controller
 {
     public function index(Request $request)
     {
+        if ($request->status == 0) {
+            $transactions = Transaction::where('userType', ROLE_DRIVER)
+                ->where('user_id', Auth::id())
+                ->whereIn('status', ['0', null])
+                ->orderByDesc('created_at')
+                ->paginate(10);
+            return response()->json($transactions, 200);
+        }
         $transactions = Transaction::where('userType', ROLE_DRIVER)
             ->where('user_id', Auth::id())
+            ->whereIn('status', ['100', '101'])
             ->orderByDesc('created_at')
             ->paginate(10);
         return response()->json($transactions, 200);
